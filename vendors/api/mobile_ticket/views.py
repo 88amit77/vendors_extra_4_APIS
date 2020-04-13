4e408fe4a35b88da61c58cd4e309c5aefac69a2b
raj.t <raj.t@pacewisdom.com>
2020-04-13 17:20:45 +0530

    Removed Django filters code

--- vendors/api/mobile_ticket/views.py
+++ vendors/api/mobile_ticket/views.py
@@ -4,7 +4,6 @@ from .models import MobileTicket,MobileTicketReply
 from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination,PageNumberPagination
 import requests
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
 class MobileTicketDetailsViewSetPagination(LimitOffsetPagination):
